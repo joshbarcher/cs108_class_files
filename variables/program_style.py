b3989d809f66055d3f6e008cdfd3010a14f3cee1
Josh Archer <jarcher@greenriver.edu>
2022-06-28 08:41:06 -0700

    Added an example file where the style can be improved.

---
+++ variables/program_style.py
@@ -0,0 +1,12 @@
+# enter a value
+v = int(input("Number: "))
+
+space = 3.14159265359 * v * v
+print(space)
+
+# enter two values
+w = int(input("Width: "))
+l = int(input("Length: "))
+
+space = l * w
+print(space)
